da5c8b3584b500d79a3ce1c4c149a1b0d13a8b9e
Manuel Schmid <manuel.schmid@odt.net>
2025-04-24 23:42:01 +0200

    feat: adjust setup, rename and add logos

--- setup.py
+++ setup.py
@@ -1,33 +1,43 @@
-from setuptools import setup
 import os
 import stat
 
-# Paths to include
+from setuptools import setup
+
+# ------------------- resource paths -------------------
 SCRIPT_SRC = os.path.join('susops', 'susops.sh')
 LOGO_FILES = [os.path.join('images', f) for f in os.listdir('images') if os.path.isfile(os.path.join('images', f))]
 
-# Ensure the shell script is executable
+ICON_FILE = os.path.join('images', 'susops.icns')
+
+# ------------------- make shell script executable -------------------
 st = os.stat(SCRIPT_SRC)
 os.chmod(SCRIPT_SRC, st.st_mode | stat.S_IEXEC)
 
-APP = ['susops-toolbar.py']
+# ------------------- py2app lists -------------------
 DATA_FILES = [
-    # Bundle the shell script under Contents/Resources/scripts/
     ('susops', [SCRIPT_SRC]),
-    # Bundle all logo assets under Contents/Resources/images/
-    ('images', LOGO_FILES),
+    ('images', LOGO_FILES + [ICON_FILE]),
 ]
 
 OPTIONS = {
     'argv_emulation': True,
+    'iconfile': ICON_FILE,
     'plist': {
-        'LSUIElement': True,
+        'CFBundleName': 'SusOps',
+        'CFBundleDisplayName': 'SusOps',
+        'CFBundleExecutable': 'app.py',
+        'LSUIElement': True,  # menu‑bar only, no dock icon
+        'CFBundleIconFile': 'susops',
     },
     'packages': ['rumps'],
 }
 
 setup(
-    app=APP,
+    name='SusOps',
+    version='1.0.0',
+    author='Manuel Schmid',
+    url='https://github.com/mashb1t/susops-mac',
+    app=["app.py"],
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
